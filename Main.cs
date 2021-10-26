6dcd16d2286f96cf5fa6bb8e7d39396d30356924
Marcus André <ddtank.marcus@gmail.com>
2021-10-26 15:11:54 -0300

    Initial Linux Port

--- Main.cs
+++ Main.cs
@@ -5,7 +5,6 @@ using SharpCompress.Archives.Rar;
 using SharpCompress.Readers;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
