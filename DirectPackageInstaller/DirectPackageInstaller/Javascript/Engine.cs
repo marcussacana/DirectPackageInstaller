b4d400ce7ef158b45b7e458fd04f99bb26e74d50
marcussacana <ddtank.marcus@gmail.com>
2024-09-07 16:01:03 -0300

    Upgrade to .NET 8

--- DirectPackageInstaller/DirectPackageInstaller/Javascript/Engine.cs
+++ DirectPackageInstaller/DirectPackageInstaller/Javascript/Engine.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Reflection;
 using Jint;
 using Jint.Native.Object;
@@ -12,8 +13,8 @@ public static class JSEngine
     public static Engine GetEngine(HtmlAgilityPack.HtmlDocument Context)
     {
         Engine JS = new Engine();
-        JS.SetValue("Engine", new ObjectWrapper(JS, JS));
-        JS.SetValue("HTML", new ObjectWrapper(JS, Context));
+        JS.SetValue("Engine", ObjectWrapper.Create(JS, JS));
+        JS.SetValue("HTML", ObjectWrapper.Create(JS, Context));
         JS.SetValue("DocumentClass", TypeReference.CreateTypeReference(JS, typeof(Document)));
         JS.Execute("this.document = new DocumentClass(Engine, HTML);");
         return JS;
